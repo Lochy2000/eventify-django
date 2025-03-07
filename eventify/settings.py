df871d5d7a382249226b8c5d2961ec7a42fd3fbf
lochy2000 <lochlann_oht@hotmail.com>
2025-03-07 11:42:10 +0000

    updated default user image to use cloudinary stored image

--- eventify/settings.py
+++ eventify/settings.py
@@ -88,7 +88,6 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from cloudinary.models import CloudinaryField
+
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -10,9 +12,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     bio = models.TextField(blank=True)
     location = models.CharField(max_length=255, blank=True)
-    avatar = models.ImageField(
-        upload_to='avatars/', default='avatars/default.jpg'
-    )
+    avatar = CloudinaryField('avatar', default='default_profile_ju9xum')
 
     class Meta:
         ordering = ['-created_at']
