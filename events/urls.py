6e04651126c6f60827fc14500a45593007f102ae
lochy2000 <lochlann_oht@hotmail.com>
2025-03-12 12:57:16 +0000

    added new attendee list and details to urls

--- events/urls.py
+++ events/urls.py
@@ -3,6 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Event URLs
     path('events/', views.EventList.as_view(), name='event-list'),
     path('events/<int:pk>/', views.EventDetail.as_view(), name='event-detail'),
+    
+    # Attendance URLs
+    path('attendees/', views.EventAttendeeList.as_view(), name='event-attendee-list'),
+    path('attendees/<int:pk>/', views.EventAttendeeDetail.as_view(), name='event-attendee-detail'),
+    path('events/<int:event_id>/attendees/', views.EventAttendeesByEvent.as_view(), name='event-attendees-by-event'),
 ]
