f61b22518c0b05aa6f725a67541d8b9d13956c31
lochy2000 <lochlann_oht@hotmail.com>
2025-03-16 17:34:01 +0000

    improve the CloudinaryField configuration in the Profile model to match the Event model

--- profiles/models.py
+++ profiles/models.py
@@ -12,13 +12,43 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     bio = models.TextField(blank=True)
     location = models.CharField(max_length=255, blank=True)
-    avatar = CloudinaryField('avatar', default='default_profile_ju9xum')
+    avatar = CloudinaryField(
+        'avatar',
+        folder='profiles',
+        blank=True,
+        null=True,
+        default='default_profile_ju9xum',
+        # Explicitly set the resource type to auto so it can handle different file types
+        resource_type='auto',
+        # Make sure transformation works
+        transformation={
+            'crop': 'fill',
+            'width': 400,
+            'height': 400
+        }
+    )
 
     class Meta:
         ordering = ['-created_at']
 
     def __str__(self):
         return f"{self.owner}'s profile"
+    
+    def save(self, *args, **kwargs):
+        """Override save method to handle Cloudinary avatar field"""
+        try:
+            # Call parent save method
+            super().save(*args, **kwargs)
+        except Exception as e:
+            # If error is related to Cloudinary, try to save without the image
+            if 'avatar' in str(e).lower() or 'cloudinary' in str(e).lower():
+                # Set avatar to default value
+                self.avatar = None
+                # Try saving again without triggering this custom save method
+                super(Profile, self).save(*args, **kwargs)
+            else:
+                # Re-raise other errors
+                raise
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
