ace4324e2f8c74762e120dcfb06de9fb05c82eaf
lochy2000 <lochlann_oht@hotmail.com>
2025-03-18 11:42:28 +0000

    set debug to false

--- eventify/settings.py
+++ eventify/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Debug mode
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST', 'eventify-back-d016873ba1b8.herokuapp.com'),
